86f16db77fecbb783a97ef63b22d1b328b8928aa
mewc <michael@piing.ai>
2025-03-15 12:47:06 +1100

    analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,27 @@ import { fontSans } from "@/config/fonts";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SJMJND9WBS"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-SJMJND9WBS');
+            `,
+          }}
+        />
+      </Head>
+
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Main />
